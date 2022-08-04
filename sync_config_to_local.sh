6534acfa8675921d64118eb26e9cff837540801b
liangyuanwei <liangyuanwei@Megvii.com>
2022-08-04 10:51:43 +0800

    daily push

--- sync_config_to_local.sh
+++ sync_config_to_local.sh
@@ -3,3 +3,4 @@ set -xeuo pipefail
 
 cp ~/my_mess/init.vim ~/.config/nvim/init.vim 
 cp ~/my_mess/.tmux.conf ~/.tmux.conf 
+cp ~/my_mess/.zshrc ~/.zshrc
--- update_config_to_my_mess.sh
+++ update_config_to_my_mess.sh
@@ -4,3 +4,4 @@ set -xeuo pipefail
 
 cp ~/.config/nvim/init.vim ~/my_mess/init.vim
 cp ~/.tmux.conf ~/my_mess/.tmux.conf
+cp ~/.zshrc ~/my_mess/.zshrc
